9813d12c2148e9d9d46f1191094cea3e76fba33d
mmnnmemie <Warinyupa.memee@gmail.com>
2022-04-03 12:29:51 +0700

    no message

--- app/src/main/java/com/example/milamix/wondercal/MainActivity.java
+++ app/src/main/java/com/example/milamix/wondercal/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         replaceFragment(new MainFragment());
+        int i = 100;
 
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
             switch (item.getItemId()){
