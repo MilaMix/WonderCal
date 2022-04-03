b575f2206d243b943672d5c450886f09f1f6e2d7
MilaMix <buntida2000@gmail.com>
2022-04-03 11:08:53 +0700

    add init

---
+++ app/src/main/java/com/example/milamix/wondercal/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.milamix.wondercal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.milamix.wondercal.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new MainFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.main:
+                    replaceFragment(new MainFragment());
+                    break;
+                case R.id.restaurant:
+                    replaceFragment(new RestaurantFragment());
+                    break;
+
+                case R.id.search:
+                    replaceFragment((new SearchFragment()));
+                    break;
+
+                case R.id.settings:
+                    replaceFragment(new SettingsFragment());
+                    break;
+            }
+
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.FrameLayout,fragment);
+        fragmentTransaction.commit();
+    }
+
+}
