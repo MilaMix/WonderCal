457d1bda65107463956ed0c9de1d733400bf18ac
mmnnmemie <Warinyupa.memee@gmail.com>
2022-04-08 20:46:58 +0700

    no message

--- app/src/main/java/com/example/milamix/wondercal/MainFragment.java
+++ app/src/main/java/com/example/milamix/wondercal/MainFragment.java
@@ -1,38 +1,26 @@
 package com.example.milamix.wondercal;
 
-import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
-import android.view.KeyboardShortcutGroup;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.TextView;
 
-import com.example.milamix.wondercal.util.DatePickerFragment;
 import com.example.milamix.wondercal.util.Utils;
 
 import java.text.DateFormat;
 import java.util.Calendar;
-import java.util.List;
-
-import okhttp3.internal.Util;
 
 
-public class MainFragment extends Fragment implements DatePickerDialog.OnDateSetListener{
+public class MainFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -42,7 +30,6 @@ public class MainFragment extends Fragment implements DatePickerDialog.OnDateSet
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-    private DatePickerDialog datePickerDialog;
     public MainFragment() {
         // Required empty public constructor
     }
@@ -73,29 +60,6 @@ public class MainFragment extends Fragment implements DatePickerDialog.OnDateSet
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        View button = (Button) getView().findViewById(R.id.loginbutton);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                DialogFragment datePicker = new DatePickerFragment();
-                datePicker.show(getParentFragmentManager(), "date picker");
-            }
-        });
-    }
-
-    @Override
-    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-        Utils.Log("asdasd");
-        Calendar c = Calendar.getInstance();
-        c.set(Calendar.YEAR, year);
-        c.set(Calendar.MONTH, month);
-        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-        String currentDateString = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
-
-        TextView dateText = (TextView) getView().findViewById(R.id.date_text);
-        dateText.setText(currentDateString);
-
     }
 }
 
