986f50f2f3e3d8e50adf93c265a209dc2c6a3948
mmnnmemie <Warinyupa.memee@gmail.com>
2022-04-16 03:09:01 +0700

    no message

---
+++ app/src/main/java/com/example/milamix/wondercal/util/SearchfoodActivity.java
@@ -0,0 +1,16 @@
+package com.example.milamix.wondercal.util;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.milamix.wondercal.R;
+
+public class SearchfoodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searchfood2);
+    }
+}
