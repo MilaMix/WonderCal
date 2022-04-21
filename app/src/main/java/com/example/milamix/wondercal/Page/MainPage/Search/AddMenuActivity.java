ab928134367891f7ca7ecf5178033fb11f1dd889
Rattanan Sakulratchata <bookbang16@gmail.com>
2022-04-22 01:26:34 +0700

    no message

--- app/src/main/java/com/example/milamix/wondercal/Page/MainPage/Search/AddMenuActivity.java
+++ app/src/main/java/com/example/milamix/wondercal/Page/MainPage/Search/AddMenuActivity.java
@@ -4,6 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.VolleyError;
 import com.example.milamix.wondercal.Models.FoodModels;
@@ -11,11 +18,14 @@ import com.example.milamix.wondercal.Models.ResponseErrorModels;
 import com.example.milamix.wondercal.Models.ResponseModels;
 import com.example.milamix.wondercal.Models.RestaurantModels;
 import com.example.milamix.wondercal.Page.LoginPage.LoginActivity;
+import com.example.milamix.wondercal.Page.MainPage.MainActivity;
 import com.example.milamix.wondercal.Page.RegisterPage.RegisterActivity;
+import com.example.milamix.wondercal.Page.UserinfoPage.UserInfoActivity;
 import com.example.milamix.wondercal.R;
 import com.example.milamix.wondercal.Service.IResult;
 import com.example.milamix.wondercal.Service.SharePref;
 import com.example.milamix.wondercal.Service.VolleyService;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -29,7 +39,11 @@ public class AddMenuActivity extends AppCompatActivity {
     IResult mResultCallback = null;
     VolleyService mVolleyService;
     JSONObject obj;
+    String[] meal_list = {"breakfast","lunch","dinner"};
 
+    AutoCompleteTextView autoCompleteTxt;
+    ArrayAdapter<String> adapterItems;
+    private String meal = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,6 +61,33 @@ public class AddMenuActivity extends AppCompatActivity {
                     bundle.getInt("cal")
             );
         }
+
+        setup();
+    }
+
+    void setup(){
+        TextView nameEn = (TextView)findViewById(R.id.addMenuFoodNameEng);
+        TextView nameTh = (TextView)findViewById(R.id.addMenuFoodNameTh);
+        ImageView img = (ImageView)findViewById(R.id.imageFood);
+
+        nameEn.setText(food.getName_en());
+        nameTh.setText(food.getName_th());
+        Picasso.get()
+                .load(food.getUrl_img())
+                .placeholder(R.mipmap.ic_launcher).fit()
+                .error(R.mipmap.ic_launcher)
+                .into(img);
+
+        autoCompleteTxt = findViewById(R.id.MealCompleteTextView);
+        adapterItems = new ArrayAdapter<String>(this,R.layout.dropdown_ac_item,meal_list);
+        autoCompleteTxt.setAdapter(adapterItems);
+        autoCompleteTxt.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                meal = parent.getItemAtPosition(position).toString();
+                Toast.makeText(getApplicationContext(), "Item: "+meal,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -54,16 +95,18 @@ public class AddMenuActivity extends AppCompatActivity {
         finish();
     }
 
-    public void addMeal(String meal) throws JSONException {
+    public void btn_add_food(View view) throws JSONException {
+
         SharePref sharePref = new SharePref(this);
 
         obj.put("email",sharePref.getString("email"));
-        obj.put("id_food",food.getId());
+        obj.put("cal",food.getCal());
+        obj.put("food",food.getName_en()+","+food.getName_th());
         obj.put("meal",meal);
 
         initVolleyCallback();
         mVolleyService = new VolleyService(mResultCallback, this);
-        mVolleyService.postDataVolleyWithToken("/food/add-food", null);
+        mVolleyService.postDataVolleyWithToken("/food/add-food", obj);
     }
 
     void initVolleyCallback(){
@@ -78,8 +121,8 @@ public class AddMenuActivity extends AppCompatActivity {
                                 .setConfirmButton("OK", new SweetAlertDialog.OnSweetClickListener() {
                                     @Override
                                     public void onClick(SweetAlertDialog sweetAlertDialog) {
-                                        swapToLoginPage();
                                         sweetAlertDialog.dismiss();
+                                        finish();
                                     }
                                 }).show();
                     }
@@ -104,6 +147,7 @@ public class AddMenuActivity extends AppCompatActivity {
             }
         };
     }
+
     private void swapToLoginPage(){
         itn = new Intent(this, LoginActivity.class);
         startActivity(itn);
