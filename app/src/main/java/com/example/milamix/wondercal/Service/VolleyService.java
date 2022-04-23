fee2909cada5abe33bbb00e9ec6b8962121e3ccd
Rattanan Sakulratchata <bookbang16@gmail.com>
2022-04-23 16:06:59 +0700

    no message

--- app/src/main/java/com/example/milamix/wondercal/Service/VolleyService.java
+++ app/src/main/java/com/example/milamix/wondercal/Service/VolleyService.java
@@ -20,7 +20,7 @@ public class VolleyService {
     IResult mResultCallback = null;
     Context mContext;
     private String TAG = "HTTPD";
-    private String host = "http://murphy.thddns.net:5150";
+    private String host = "http://murphy.thddns.net:5151";
 
     public VolleyService(IResult resultCallback, Context context){
         mResultCallback = resultCallback;
